24039d9357c8a15a3a49a0830ae69caf09747f49
David-Vilches <davidvilches388@gmail.com>
2026-05-19 13:08:55 +0200

    primercommit

---
+++ Pr4EntornosJa/src/main/java/com/david/pr4entornosja/Pr4EntornosJa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.david.pr4entornosja;
+
+/**
+ *
+ * @author medac
+ */
+public class Pr4EntornosJa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
